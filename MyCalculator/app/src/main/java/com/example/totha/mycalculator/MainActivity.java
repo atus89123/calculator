9fbb104737bd8c15f53e6f8c18fbdadf353ebbff
LZsolt <zsolt.laszlo@p92rdi.hu>
2016-07-21 11:36:27 +0200

    new algoritm, didnt work out so commented it

--- MyCalculator/app/src/main/java/com/example/totha/mycalculator/MainActivity.java
+++ MyCalculator/app/src/main/java/com/example/totha/mycalculator/MainActivity.java
@@ -5,36 +5,45 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.Stack;
+import java.util.TreeSet;
 
-
-/*          TO_DO           */
-/*  - AUTOMATIC SRCOLL DOWN
-    - GRIDLAYOUT SIZE ELEMNTS   */
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView resultTextView;
     StringBuilder rawStringBuilder;
-    Boolean resultIsCalculated;
+    boolean resultIsCalculated;
+    /*String lastInput;
+    Set<String> operatorSet;
+    Stack<String> previousInputs;*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultTextView = (TextView) findViewById(R.id.resultTextView);
-        GridLayout buttonGridLayout = (GridLayout) findViewById(R.id.buttonGridLayout);
 
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
         resultTextView.setMovementMethod(new ScrollingMovementMethod());
 
         rawStringBuilder = new StringBuilder();
         resultIsCalculated = false;
+        /*lastInput = "";
+
+        previousInputs = new Stack<>();
 
         resultTextView.setText("0");
+
+        operatorSet = new TreeSet<>();
+        operatorSet.add("+");
+        operatorSet.add("-");
+        operatorSet.add("*");
+        operatorSet.add("/");*/
     }
 
 
@@ -43,36 +52,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button button =(Button)v;
         String buttonText = button.getText().toString();
+        //lastInput = buttonText;
 
-        switch(buttonText){
+        switch(buttonText) {
 
             case "=":
                 String finalString = rawStringBuilder.toString();
-                if(finalString.equals("")) break;
-                double res = reversePolishForm(polishForm(rawStringBuilder.toString()));
+                double res = reversePolishForm(polishForm(finalString));
                 resultTextView.setText(finalString + " = " + res);
                 rawStringBuilder.setLength(0);
                 rawStringBuilder.append(res);
                 resultIsCalculated = true;
                 break;
-            case "+": case "-": case "*": case "/":
+            case "+":
+            case "-":
+            case "*":
+            case "/":
                 resultIsCalculated = false;
                 validateOperator(rawStringBuilder, buttonText);
-
                 break;
             case ".":
                 String[] tmp = rawStringBuilder.toString().split("[^\\d\\.\\d]");
-                if(tmp.length != 0 && !tmp[tmp.length-1].contains(".")) validateOperator(rawStringBuilder, buttonText);
-
+                if (tmp.length != 0 && !tmp[tmp.length - 1].contains("."))
+                    validateOperator(rawStringBuilder, buttonText);
                 break;
             case "Clear":
                 resultIsCalculated = false;
                 rawStringBuilder.setLength(0);
                 resultTextView.setText("0");
-
                 break;
             default:
-                if(resultIsCalculated) {
+                if (resultIsCalculated) {
                     resultIsCalculated = false;
                     rawStringBuilder.setLength(0);
                 }
@@ -80,9 +90,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 resultTextView.setText(rawStringBuilder.toString());
                 break;
         }
+
+        /*if(buttonText.equals("Delete"))
+        {
+            deleteLastCharacter();
+        }
+        else if(buttonText.equals("Clear"))
+        {
+            rawStringBuilder.setLength(0);
+            lastInput = "";
+            previousInputs.clear();
+            resultTextView.setText("0");
+        }
+        else if(operatorSet.contains(buttonText) && !previousInputs.isEmpty())
+        {
+            if(operatorSet.contains(lastInput) || lastInput.equals(".")) deleteLastCharacter();
+            showAndSaveNewInput(buttonText);
+        }
+        else if(buttonText.equals(".") && checkForOneDotPerExpr())
+        {
+            showAndSaveNewInput(buttonText);
+        }
+        else if(buttonText.equals("="))
+        {
+            String finalString = rawStringBuilder.toString();
+            double result = reversePolishForm(polishForm(finalString));
+            resultTextView.setText(finalString + " = " + result);
+            rawStringBuilder.setLength(0);
+            lastInput = "";
+            previousInputs.clear();
+
+        }
+        else
+        {
+            showAndSaveNewInput(buttonText);
+        }*/
+    }
+
+    private void validateOperator(StringBuilder rawStringBuilder, String buttonText) {
+        if (rawStringBuilder.length() != 0) {
+            char lastChar = rawStringBuilder.charAt(rawStringBuilder.length() - 1);
+            if ((lastChar == '+') || (lastChar == '-') || (lastChar == '*') ||
+                    (lastChar == '/') || lastChar == '.') {
+                rawStringBuilder.deleteCharAt(rawStringBuilder.length() - 1);
+            }
+            rawStringBuilder.append(buttonText);
+            resultTextView.setText(rawStringBuilder.toString());
+        }
     }
 
     public double reversePolishForm(ArrayList<String> components) {
+        if(components.isEmpty()) return 0;
         Stack<Double> stack = new Stack<Double>();;
         double num1;
         double num2;
@@ -115,18 +173,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return stack.pop();
     }
 
-    private void validateOperator(StringBuilder rawStringBuilder, String buttonText) {
-        if (rawStringBuilder.length() != 0) {
-            char lastChar = rawStringBuilder.charAt(rawStringBuilder.length() - 1);
-            if ((lastChar == '+') || (lastChar == '-') || (lastChar == '*') ||
-                    (lastChar == '/') || lastChar == '.') {
-                rawStringBuilder.deleteCharAt(rawStringBuilder.length() - 1);
-            }
-            rawStringBuilder.append(buttonText);
-            resultTextView.setText(rawStringBuilder.toString());
-        }
+    /*
+    private void showAndSaveNewInput(String newInput){
+        rawStringBuilder.append(newInput);
+        previousInputs.push(lastInput);
+        lastInput = newInput;
+        resultTextView.setText(rawStringBuilder.toString());
     }
 
+    private boolean checkForOneDotPerExpr()
+    {
+        String[] tmp = rawStringBuilder.toString().split("[^\\d\\.\\d]");
+        return (tmp.length != 0 && !tmp[tmp.length-1].contains(".") && !operatorSet.contains(lastInput));
+    }
+
+    private void deleteLastCharacter()
+    {
+        int lengthOfRawString = rawStringBuilder.length();
+        if(lengthOfRawString > 0 && !previousInputs.isEmpty()) {
+            rawStringBuilder.deleteCharAt(lengthOfRawString - 1);
+            previousInputs.pop();
+            lastInput = previousInputs.peek();
+        }
+    }*/
 
     public ArrayList<String> polishForm(String s){
         int i = 0;
