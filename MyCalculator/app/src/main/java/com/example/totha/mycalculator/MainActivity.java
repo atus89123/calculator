50f4027701c432155cd7a21094ed1eb8aeae63ae
atus89123 <toth.ata11@gmail.com>
2016-07-22 10:18:16 +0200

    merged

--- MyCalculator/app/src/main/java/com/example/totha/mycalculator/MainActivity.java
+++ MyCalculator/app/src/main/java/com/example/totha/mycalculator/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.totha.mycalculator;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
